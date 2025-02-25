c5693cdb43dc6eb857a76f2dd55ab29825cdbe44
Vadim <vadimfedulov035@gmail.com>
2025-02-25 19:29:39 +0300

    v1.2

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -1,88 +1,43 @@
 local M = {}
 
--- Helper function for buffer-local mappings
-local create_map = function(bufnr)
-	return function(mode, lhs, rhs, desc, opts)
-		local options = vim.tbl_extend("force", {
-			buffer = bufnr,
-			noremap = true,
-			silent = true,
-			desc = desc,
-		}, opts or {})
-
-		vim.keymap.set(mode, lhs, rhs, options)
-	end
-end
-
 -- Core LSP on_attach
-M.lspcore_on_attach = function(client, bufnr)
-	local map = create_map(bufnr)
-
-	-- LSP Actions
-	map("n", "K",  vim.lsp.buf.hover,       "Hover Documentation")
-	map("n", "gd", vim.lsp.buf.definition,  "Goto Definition")
-	map("n", "gr", vim.lsp.buf.references,  "Goto References")
-	map("n", "rn", vim.lsp.buf.rename,      "Rename Symbol")
-	map("n", "ca", vim.lsp.buf.code_action, "Code Actions")
-	map("n", "f",  vim.lsp.buf.format,      "Format Buffer")
-
-	-- Signature Help
-	require("lsp_signature").on_attach({
-		bind = true,
-		handler_opts = {
-			border = "rounded"
-		}
-	}, bufnr)
-end
-
--- LSP Saga key mappings
-M.lspsaga_on_attach = function(client, bufnr)
-	local map = create_map(bufnr)
-	local saga = require('lspsaga')
-
-	-- Peek definitions
-	map("n", "pd", saga.peek_definition, "Preview Definition")
-	map("n", "pt", saga.peek_type_definition, "Preview Type Definition")
-	-- Finder and implementations
-	map("n", "pi", saga.lsp_finder, "Implementation Finder")
-	map("n", "pr", saga.find_ref, "Find References")
-	-- Code actions
-	map({"n", "v"}, "<leader>ca", saga.code_action, "Code Action")
-	-- Diagnostics
-	map("n", "[e", saga.diagnostic.goto_prev, "Previous Diagnostic")
-	map("n", "]e", saga.diagnostic.goto_next, "Next Diagnostic")
-	-- Floating window management
-	map("n", "<Esc>", saga.action.close_all_floats, "Close All Floats")
-end
-
--- LSP key mappings
 M.lsp_on_attach = function(client, bufnr)
-	-- Set omnifunc for LSP-based completion
 	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-	M.lspcore_on_attach(client, bufnr)
-	M.lspsaga_on_attach(client, bufnr)
+	-- LSP Actions
+	vim.keymap.set("n", "K",  vim.lsp.buf.hover, { buffer = bufnr})
+
+	-- Goto definitions
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer = bufnr})
+	vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer = bufnr})
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer = bufnr})
+
+	-- Peek definitions
+	vim.keymap.set("n", "pd", "<cmd>Lspsaga peek_definition<CR>", {buffer = bufnr})
+	vim.keymap.set("n", "pt", "<cmd>Lspsaga peek_type_definition<CR>", {buffer = bufnr})
+	vim.keymap.set("n", "pi", "<cmd>Lspsaga peek_implementation<CR>", {buffer = bufnr})
+	-- Floating window management
+	vim.keymap.set("n", "<Esc>", saga.action.close_all_floats, {buffer = bufnr})
 end
 
 --- EXTRA KEY MAPPINGS (import in module)
 -- GitSigns key mappings
 M.gitsigns_on_attach = function(_, bufnr)
-	local map = create_map(bufnr)
 	local gs = package.loaded.gitsigns
 
-	map("n", "]c", gs.next_hunk, "Next Hunk")
-	map("n", "[c", gs.prev_hunk, "Previous Hunk")
-	map("n", "<leader>hs", gs.stage_hunk, "Stage Hunk")
-	map("n", "<leader>hr", gs.reset_hunk, "Reset Hunk")
-	map("v", "<leader>hs", function() gs.stage_hunk({vim.fn.line("."), vim.fn.line("v")}) end, "Stage Visual Hunk")
-	map("v", "<leader>hr", function() gs.reset_hunk({vim.fn.line("."), vim.fn.line("v")}) end, "Reset Visual Hunk")
-	map("n", "<leader>hS", gs.stage_buffer, "Stage Buffer")
-	map("n", "<leader>hu", gs.undo_stage_hunk, "Undo Stage")
-	map("n", "<leader>hR", gs.reset_buffer, "Reset Buffer")
-	map("n", "<leader>hp", gs.preview_hunk, "Preview Hunk")
-	map("n", "<leader>hb", function() gs.blame_line({ full = true }) end, "Blame Line")
-	map("n", "<leader>hd", gs.diffthis, "Diff This")
-	map("n", "<leader>hD", function() gs.diffthis("~") end, "Diff Against HEAD~")
+	vim.keymap.set("n", "]c", gs.next_hunk, "Next Hunk")
+	vim.keymap.set("n", "[c", gs.prev_hunk, "Previous Hunk")
+	vim.keymap.set("n", "<leader>hs", gs.stage_hunk, "Stage Hunk")
+	vim.keymap.set("n", "<leader>hr", gs.reset_hunk, "Reset Hunk")
+	vim.keymap.set("v", "<leader>hs", function() gs.stage_hunk({vim.fn.line("."), vim.fn.line("v")}) end, "Stage Visual Hunk")
+	vim.keymap.set("v", "<leader>hr", function() gs.reset_hunk({vim.fn.line("."), vim.fn.line("v")}) end, "Reset Visual Hunk")
+	vim.keymap.set("n", "<leader>hS", gs.stage_buffer, "Stage Buffer")
+	vim.keymap.set("n", "<leader>hu", gs.undo_stage_hunk, "Undo Stage")
+	vim.keymap.set("n", "<leader>hR", gs.reset_buffer, "Reset Buffer")
+	vim.keymap.set("n", "<leader>hp", gs.preview_hunk, "Preview Hunk")
+	vim.keymap.set("n", "<leader>hb", function() gs.blame_line({ full = true }) end, "Blame Line")
+	vim.keymap.set("n", "<leader>hd", gs.diffthis, "Diff This")
+	vim.keymap.set("n", "<leader>hD", function() gs.diffthis("~") end, "Diff Against HEAD~")
 end
 
 -- Completion key mappings
